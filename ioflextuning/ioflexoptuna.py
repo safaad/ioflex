6fc4a408b09c7a388109a57841daa99df55ec0e8
Safaa Diab <safaa_dyab@live.com>
2024-08-07 15:41:32 +0200

    Add Optuna

--- ioflextuning/ioflexoptuna.py
+++ ioflextuning/ioflexoptuna.py
@@ -1,5 +1,8 @@
-#pip install hyperopt, pyll for gp
-from hyperopt import fmin, tpe, Trials, rand, hp, atpe, anneal, rand
+# !pip install optuna
+# Best Samplers for Optuna  Categorial RandSampler, GridSampler, TPESampler, NSGAIISampler, GPSampler, BruteForceSampler, Simulated Annealing
+# GP requires torch  
+import optuna
+import logging
 import subprocess
 import shutil
 import os
@@ -38,7 +41,18 @@ iter_count = 0
 # List of files to clean after running application
 files_to_clean = []
 
-def eval_func(params):
+
+# Configure Optuna logging to stdout
+optuna.logging.set_verbosity(optuna.logging.INFO)
+logger = logging.getLogger()
+handler = logging.StreamHandler(sys.stdout)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+logger.setLevel(logging.INFO)
+
+
+def eval_func(trial):
 
     dir_path = os.environ['PWD']
 
@@ -51,15 +65,15 @@ def eval_func(params):
 
     # Set IO Configurations
     configs_str=""
-    if strf_id is not None:
-        strf_val = params['striping_factor']
+    if len(striping_factor):
+        strf_val = trial.suggest_categorical('striping_factor', striping_factor)
         if ioflexset:
             config_file.write("striping_factor = " + str(strf_val)+"\n")
         else:
             config_file.write("striping_factor " + str(strf_val)+"\n")
         configs_str += "striping_factor," + str(strf_val) + ","
-    if stru_id is not None:
-        stru_val = params['striping_unit']
+    if len(striping_unit):
+        stru_val = trial.suggest_categorical('striping_unit', striping_unit)
         if ioflexset:
             config_file.write("striping_unit = " + str(stru_val)+"\n")
             config_file.write("cb_buffer_size = " + str(stru_val)+"\n")
@@ -67,51 +81,51 @@ def eval_func(params):
             config_file.write("striping_unit " + str(stru_val)+"\n")
             config_file.write("cb_buffer_size " + str(stru_val)+"\n")
         configs_str += "striping_unit," + str(stru_val) + ",cb_buffer_size," + str(stru_val) + ","  
-    if cbn_id is not None:
-        cbn_val = params['cb_nodes']
+    if len(cb_nodes):
+        cbn_val = trial.suggest_categorical('cb_nodes', cb_nodes)
         if ioflexset:
             config_file.write("cb_nodes = " + str(cbn_val)+"\n")
         else:
             config_file.write("cb_nodes " + str(cbn_val)+"\n")
         configs_str += "cb_nodes," + str(cbn_val) + ","
-    if fstype_id is not None:
-        fstype_val = params['romio_fs_type']
+    if len(romio_fs_type):
+        fstype_val = trial.suggest_categorical('romio_fs_type', romio_fs_type)
         os.environ["ROMIO_FSTYPE_FORCE"]=fstype_val
         if ioflexset:
             config_file.write("romio_filesystem_type = " + fstype_val+"\n")
         else:
             config_file.write("romio_filesystem_type " + fstype_val+"\n")
         configs_str += "romio_filesystem_type," + fstype_val + ","
-    if dsread_id is not None:
-        dsread_val = params['romio_ds_read']
+    if len(romio_ds_read):
+        dsread_val = trial.suggest_categorical('romio_ds_read', romio_ds_read)
         if ioflexset:
             config_file.write("romio_ds_read = " + dsread_val+"\n")
         else:
             config_file.write("romio_ds_read " + dsread_val+"\n")
         configs_str += "romio_ds_read," + dsread_val + ","
-    if dswrite_id is not None:
-        dswrite_val = params["romio_ds_write"]
+    if len(romio_ds_write):
+        dswrite_val = trial.suggest_categorical('romio_ds_write', romio_ds_write)
         if ioflexset:
             config_file.write("romio_ds_write = " + dswrite_val+"\n")
         else:
             config_file.write("romio_ds_write " + dswrite_val+"\n")
         configs_str += "romio_ds_write," + dswrite_val + ","
-    if cbread_id is not None:
-        cbread_val = params['romio_cb_read']
+    if len(romio_cb_read):
+        cbread_val = trial.suggest_categorical('romio_cb_read', romio_cb_read)
         if ioflexset:
             config_file.write("romio_cb_read = " + cbread_val+"\n")
         else:
             config_file.write("romio_cb_read " + cbread_val+"\n")
         configs_str += "romio_cb_read," + cbread_val + ","
-    if cbwrite_id is not None:
-        cbwrite_val = params['romio_cb_write']
+    if len(romio_cb_write):
+        cbwrite_val = trial.suggest_categorical('romio_cb_write', romio_cb_write)
         if ioflexset:
             config_file.write("romio_cb_write = " + cbwrite_val+"\n")
         else:
             config_file.write("romio_cb_write " + cbwrite_val +"\n")
         configs_str += "romio_cb_write," + cbwrite_val + ","
-    if cbl_id is not None:
-        cbl_val = params['cb_config_list']
+    if len(cb_config_list):
+        cbl_val = trial.suggest_categorical('cb_config_list', cb_config_list)
         if ioflexset:
             config_file.write("cb_config_list = " + cbl_val+"\n")
         else:
@@ -156,18 +170,19 @@ def eval_func(params):
 
 configs_space = {}
 
-def ioflexhyperopt():
+def ioflexoptuna():
     ap = argparse.ArgumentParser(add_help=True)
     ap.add_argument('--ioflex', action="store_true", default=False,help="IOFlex is enabled")
 
     ## TODO
-    ap.add_argument('--outfile', type=str, help="Path to output CSV file", default="./OutIOFlexHyperopt.csv")
-    ap.add_argument('--inhyperopt', type=str, help="Hyperopt instance pickled file from previous runs", default=None)
-    ap.add_argument('--outhyperopt', '-o', type=str, help="Hyperopt instance output pkl file", default="./hyperopt_trails.pkl")
+    ap.add_argument('--outfile', type=str, help="Path to output CSV file", default="./OutIOFlexOptuna.csv")
+    ap.add_argument('--inoptuna', type=str, help="Optuna study pickled file from previous runs", default=None)
+    ap.add_argument('--outoptuna', '-o', type=str, help="Optuna study output pkl file", default="./optuna_study.pkl")
     ap.add_argument('--cmd', '-c', type=str, required=True, action="store", nargs="*", help="Application command line")
-    # Hyperopt Options
-    ap.add_argument("--max_evals", type=int, help="Max number of evaluations",default=50)
-    ap.add_argument("--algorithm", type=str, help="Algorithm used by Hyperopt possible options: tpe (Tree-structured Parzen Estimator), apte (Adaptive TPE), rand (Random Search), anneal (Simulated Annealing), gp (BO using Gaussian Processes)", default="tpe")
+    # Optuna Options
+    ap.add_argument("--max_trials", type=int, help="Max number of trials", default=50)
+    ap.add_argument("--sampler", type=str, help="Sampler used by Optuna supported samplers: tpe (Tree-structured Parzen Estimator), rand (Random Search), gp (BO using Gaussian Processes), nsga (NSGAII), brute (Brute Force (all possibilities), grid (Grid))", default="tpe")
+    ap.add_argument("--pruner", type=str, help="Pruner used by optuna", default=None)
     ap.add_argument("--with_log_path", type=str, help="If set the output of the program will be saved in this path (Default:None)", default=None)
     parse = ap.parse_args()
     args = vars(parse)
@@ -181,60 +196,10 @@ def ioflexhyperopt():
     global outfile
     outfile = open(args["outfile"], 'w')
 
-    print("Starting HyperOpt Tuning")
+    print("Starting Optuna Tuning")
 
-    global max_evals, iter_count
+    global max_trials, iter_count
 
-    global no_of_configs
-    no_of_configs = 0
-
-    global strf_id, stru_id, cbn_id, fstype_id, dsread_id, dswrite_id, cbread_id, cbwrite_id, cbl_id
-    strf_id = None
-    stru_id = None
-    cbn_id = None
-    fstype_id = None
-    dsread_id = None
-    dswrite_id = None
-    cbread_id = None
-    cbwrite_id = None
-    cbl_id = None
-
-    if (len(striping_factor)):
-        configs_space.update({'striping_factor':hp.choice('striping_factor', striping_factor)})
-        strf_id = no_of_configs
-        no_of_configs += 1
-    if (len(striping_unit)):
-        configs_space.update({'striping_unit':hp.choice('striping_unit', striping_unit)})
-        stru_id = no_of_configs     
-        no_of_configs += 1
-    if (len(cb_nodes)):
-        configs_space.update({'cb_nodes':hp.choice('cb_nodes', cb_nodes)})
-        cbn_id = no_of_configs
-        no_of_configs += 1
-    if (len(romio_fs_type)):
-        configs_space.update({'romio_fs_type':hp.choice('romio_fs_type', romio_fs_type)})
-        fstype_id = no_of_configs
-        no_of_configs += 1
-    if (len(romio_ds_read)):
-        configs_space.update({'romio_ds_read':hp.choice('romio_ds_read', romio_ds_read)})
-        dsread_id = no_of_configs
-        no_of_configs += 1
-    if (len(romio_ds_write)):
-        configs_space.update({'romio_ds_write':hp.choice('romio_ds_write', romio_ds_write)})
-        dswrite_id = no_of_configs
-        no_of_configs += 1
-    if (len(romio_cb_read)):
-        configs_space.update({'romio_cb_read':hp.choice('romio_cb_read', romio_cb_read)})
-        cbread_id = no_of_configs
-        no_of_configs += 1
-    if (len(romio_cb_write)):
-        configs_space.update({'romio_cb_write':hp.choice('romio_cb_write',romio_cb_write)})
-        cbwrite_id  = no_of_configs
-        no_of_configs += 1 
-    if (len(cb_config_list)):
-        configs_space.update({'cb_config_list':hp.choice('cb_config_list',cb_config_list)})
-        cbl_id = no_of_configs
-        no_of_configs += 1
 
     global applogpath, logisset, logfile_o, logfile_e
 
@@ -250,45 +215,78 @@ def ioflexhyperopt():
         logisset = False
         print("The output of the app will not be logged")
 
-    max_evals = args['max_evals']
+    max_trials = args['max_trials']
 
-    algo=tpe.suggest
-    
-    match args["algorithm"]:
-        case "atpe":
-            print("Running with ATPE")
-            algo = atpe.suggest
-        case "anneal":
-            print("Running with Simulated Annealing ")
-            algo = anneal.suggest
+    # Define search space for grid  sampler
+    config_space = {}
+
+    if len(striping_factor):
+        config_space['striping_factor'] = striping_factor
+    if len(striping_unit):
+        config_space['striping_unit'] = striping_unit
+    if len(cb_nodes):
+        config_space['cb_nodes'] = cb_nodes
+    if len(romio_fs_type):
+        config_space['romio_fs_type'] = romio_fs_type
+    if len(romio_ds_read):
+        config_space['romio_ds_read'] = romio_ds_read
+
+    if len(romio_ds_write):
+        config_space['romio_ds_write'] = romio_ds_write
+
+    if len(romio_cb_read):
+        config_space['romio_cb_read'] = romio_cb_read
+
+    if len(romio_cb_write):
+        config_space['romio_cb_write'] = romio_cb_write
+
+    if len(cb_config_list):
+        config_space['cb_config_list'] = cb_config_list
+
+
+    print(config_space)
+    sampler = optuna.samplers.TPESampler()
+    match args["sampler"]:
+        case "gp":
+            print("Running with Gaussian Processes")
+            sampler = optuna.samplers.GPSampler()
         case "rand":
             print("Running with Random Search")
-            algo = rand.suggest
-        case "gp":
-            print("Running with Bayesian Optimization using Gaussian Processes")
-            algo = gp.suggest
-        case "tpe":
-            print("Running with TPE")
-            algo = tpe.suggest
+            sampler = optuna.samplers.RandomSampler()
+        case "nsga":
+            print("Running with NSGA-II")
+            sampler = optuna.samplers.NSGAIISampler()
+        case "grid":
+            print("Running with Grid Search")
+            sampler = optuna.samplers.GridSampler(config_space)
+        case "brute":
+            print("Running with Brute Force")
+            sampler = optuna.samplers.BruteForceSampler()
 
-    # Load previous hyperopt instance
-    if args["inhyperopt"] is None:
-        trials = Trials()
+    # Load previous optuna instance
+    if args["inoptuna"] is None:
+        study = optuna.create_study(direction='minimize', sampler=sampler, study_name='optuna_study')
     else:
-        with open(args["inhyperopt"], 'rb') as infile:
-            trials = pickle.load(infile)
+        with open(args["inoptuna"], 'rb') as f:
+            study = pickle.load(f)
+
     
     # Perform Optimization
+    study.optimize(eval_func, n_trials=args["max_trials"])
 
-    best = fmin(fn=eval_func, space=configs_space, algo=algo, max_evals=max_evals, trials=trials)
-
-    print("Best parameters: ", best)
+    # Print the best trial
+    print("Best trial:")
+    trial = study.best_trial
+    print(f"  Time: {trial.value}")
+    print(f"  Params: ")
+    for key, value in trial.params.items():
+        print(f"    {key}: {value}")
 
     print("Saving Results")
 
 
-    with open(args["outhyperopt"], 'wb') as f:
-        pickle.dump(trials, f)
+    with open(args["outoptuna"], 'wb') as f:
+        pickle.dump(study, f)
 
     outfile.close()
 
@@ -297,4 +295,4 @@ def ioflexhyperopt():
         logfile_e.close()
 
 if __name__ == '__main__':
-    ioflexhyperopt()
+    ioflexoptuna()
